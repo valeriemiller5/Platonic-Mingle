802842b0739d6928e51772d0f3acea4e04e9064d
teamjuli0 <juliovaldez@zoho.com>
2018-11-17 13:57:42 -0800

    Simple Login Page

--- reactNative/App.js
+++ reactNative/App.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+// import { createStackNavigator, AppRegistry, createMaterialTopTabNavigator, createAppContainer } from 'react-navigation'
+// import Home from './views/Home'
+// import Details from './views/Details'
+// import Results from './views/Results'
+import Login from './views/Login'
 
-export default class HelloWorldApp extends Component {
+export default class ReactNativeCommonScreen extends Component {
   render() {
-    return(
-      <View>
-        <Text>Hello World!</Text>
-      </View>
-    )
+    return <Login />
   }
 }
+
+// const HomeStack = createStackNavigator({
+//   HomeList: {
+//     screen: Home,
+//     navigationOptions: {
+//       title: 'Search Topic'
+//     }
+//   },
+//   Details: {
+//     screen: Details,
+//     navigationOptions: ({ navigation }) => ({
+//       title: `${navigation.state.params.title}`
+//     })
+//   }
+// })
+
+// const ResultsStack = createStackNavigator ({
+//   ResultsList: {
+//     screen: Results,
+//     navigationOptions: {
+//       title: `Results`
+//     },
+//     ResultsDetails: {
+//       screen: Results,
+//       navigationOptions: ({ navigation }) => ({
+//         title: `${navigation.state.params.title}`
+//       })
+//     }
+//   }
+// })
+
+// export default createAppContainer(createMaterialTopTabNavigator({
+//   Home: { screen: HomeStack },
+//   Results: { screen: ResultsStack }
+// }))
---
+++ reactNative/views/Login.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { StyleSheet, TextInput, ScrollView } from 'react-native'
+
+import Container from '../components/Container'
+import Button from '../components/Button'
+import Label from '../components/Label'
+
+export default class Login extends Component {
+  render() {
+    return (
+      <ScrollView style={styles.scroll}>
+        <Container>
+          <Label text="Username or Email" />
+          <TextInput style={styles.textInput} />
+        </Container>
+        <Container>
+          <Label text="Password" />
+          <TextInput secureTextEntry={true} style={styles.textInput} />
+        </Container>
+        <Container>
+          <Button
+            label="Forgot Password"
+            styles={{ button: StyleSheet.alignRight, label: styles.label }}
+          />
+        </Container>
+      </ScrollView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  scroll: {
+    backgroundColor: '#AFC5CF',
+    paddingTop: 100,
+    padding: 30,
+    flexDirection: 'column'
+  },
+  label: {
+    color: '#0d8898',
+    fontSize: 20
+  },
+  alignRight: {
+    alignSelf: 'flex-end'
+  }
+})
